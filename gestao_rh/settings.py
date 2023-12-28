8a7fa7aae5206dfd226b25ccdaba863c82130293
Ricardo Sousa <r.sousa.888@gmail.com>
2023-12-28 12:27:44 +0000

    criando aplicaçao departamentos

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.empresas.apps.EmpresasConfig',
+    'apps.funcionarios.apps.FuncionariosConfig',
+    'apps.departamentos.apps.DepartamentosConfig'
 ]
 
 MIDDLEWARE = [
